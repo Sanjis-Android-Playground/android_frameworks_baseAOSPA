ec92a37f66a454963bf6c1deaacd3c3f4220b125
Sanjivns <alshahab109@gmail.com>
2025-01-29 11:17:37 +0000

    fixup! a wrong pick

--- packages/SettingsProvider/src/android/provider/settings/backup/SystemSettings.java
+++ packages/SettingsProvider/src/android/provider/settings/backup/SystemSettings.java
@@ -117,8 +117,7 @@ public class SystemSettings {
                 Settings.System.NOTIFICATION_COOLDOWN_ALL,
                 Settings.System.NOTIFICATION_COOLDOWN_VIBRATE_UNLOCKED,
                 Settings.System.RINGTONE_VIBRATION_PATTERN,
-                Settings.System.NOTIFICATION_VIBRATION_PATTERN
-                Settings.System.THREE_FINGER_GESTURE,
+                Settings.System.NOTIFICATION_VIBRATION_PATTERN,
                 Settings.System.NAVBAR_LONG_PRESS_GESTURE
         ));
         if (Flags.backUpSmoothDisplayAndForcePeakRefreshRate()) {
