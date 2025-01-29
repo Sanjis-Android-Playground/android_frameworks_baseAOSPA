82582192f7a0de14c40fc7e472158ebd94d515bf
Sanjivns <alshahab109@gmail.com>
2025-01-29 18:14:17 +0000

    fixup

--- packages/SettingsProvider/src/android/provider/settings/backup/SystemSettings.java
+++ packages/SettingsProvider/src/android/provider/settings/backup/SystemSettings.java
@@ -117,9 +117,8 @@ public class SystemSettings {
                 Settings.System.NOTIFICATION_COOLDOWN_ALL,
                 Settings.System.NOTIFICATION_COOLDOWN_VIBRATE_UNLOCKED,
                 Settings.System.RINGTONE_VIBRATION_PATTERN,
-                Settings.System.NOTIFICATION_VIBRATION_PATTERN
-                Settings.System.THREE_FINGER_GESTURE,
-                Settings.System.NAVBAR_LONG_PRESS_GESTURE
+                Settings.System.NOTIFICATION_VIBRATION_PATTERN,
+                Settings.System.NAVBAR_LONG_PRESS_GESTURE,
                 Settings.System.NETWORK_TRAFFIC_STATE,
                 Settings.System.NETWORK_TRAFFIC_AUTOHIDE_THRESHOLD
        ));
